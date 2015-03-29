ffae999525ae2d39501c05a823363c299bb726e6
Alexandre <alexandrecmoraes89@gmail.com>
2015-03-29 01:04:47 -0300

    Added get_salesforce_[accounts/contacts] to get a list of current objects in Salesforce

--- lib/digitalforce/concerns/account.rb
+++ lib/digitalforce/concerns/account.rb
@@ -14,27 +14,32 @@ module Digitalforce
       end
 
       module ClassMethods
+
         def acts_as_account(options = {})
           cattr_accessor :client
           self.client = Digitalforce::Base::Connection.connect options[:connection]
         end
+
+        def get_salesforce_accounts
+          cattr_accessor :client
+          self.client.query("SELECT Id, name FROM Account")
+        end
+
       end
 
       def manage_salesforce_account_creation
-        if self.new_record?
+        if self.new_record? && !self.s_id?
           @accountId = self.client.create('Account', Name: self.name)
           self.s_id = @accountId
         else
           self.client.update('Account', Id: self.s_id, Name: self.name)
         end
-        true
       end
 
       def manage_salesforce_account_destroy
         if self.s_id
           self.client.destroy('Account', self.s_id)
         end
-        true
       end
 
     end
--- lib/digitalforce/concerns/contact.rb
+++ lib/digitalforce/concerns/contact.rb
@@ -14,27 +14,32 @@ module Digitalforce
       end
 
       module ClassMethods
+
         def acts_as_contact(options = {})
           cattr_accessor :client
           self.client = Digitalforce::Base::Connection.connect options[:connection]
         end
+
+        def get_salesforce_contacts
+          cattr_accessor :client
+          self.client.query("SELECT Id, firstName, email, lastName, accountId, phone FROM Contact")
+        end
+
       end
 
       def manage_salesforce_contact_creation
-        if self.new_record?
-          @userId = self.client.create('Contact', FirstName: self.name, LastName: self.last_name, AccountId: self.account_id, Email: self.email, Phone: self.phone)
-          self.s_id = @userId
+        if self.new_record? && !self.s_id?
+            @userId = self.client.create('Contact', FirstName: self.name, LastName: self.last_name, AccountId: self.account_id, Email: self.email, Phone: self.phone)
+            self.s_id = @userId
         else
           self.client.update('Contact', Id: self.s_id, FirstName: self.name, LastName: self.last_name, AccountId: self.account_id, Email: self.email, Phone: self.phone)
         end
-        true
       end
 
       def manage_salesforce_contact_destroy
         if self.s_id?
           self.client.destroy('Contact', self.s_id)
         end
-        true
       end
 
     end
